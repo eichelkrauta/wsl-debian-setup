cd29a479e741c23882b9850a61940ff0ef0739a4
Adam Eichelkraut <eichelkrautadam@gmail.com>
2020-10-11 16:16:09 -0500

    Java jdk 15, set JAVA_HOME to jdk-15

--- setup.sh
+++ setup.sh
@@ -11,4 +11,8 @@ sudo apt-get install zsh git wget
 
 mkdir -p ~/.ssh
 
+# Java JDK
+sudo mkdir -p /usr/local/lib/java/15
+sudo wget -qO- https://download.java.net/java/GA/jdk15/779bf45e88a44cbd9ea6621d33e33db1/36/GPL/openjdk-15_linux-x64_bin.tar.gz | sudo tar xvz -C /usr/local/lib/java
+
 cp ./zshrc ~/.zshrc
