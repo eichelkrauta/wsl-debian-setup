b66c04d3a40225275cfbb8be0b46b65179c3b179
Adam Eichelkraut <eichelkrautadam@gmail.com>
2020-11-15 11:57:47 -0600

    dont worry too much about idempotency, just worry about installing things

--- setup.sh
+++ setup.sh
@@ -5,15 +5,12 @@ set -euo pipefail
 sudo apt-get update
 sudo apt-get install zsh git wget
 
-## todo how to reinstall zshell?
-##rm -rf /home/adame/.oh-my-zsh
-##sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-#
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 mkdir -p ~/.ssh
-#
-## Java JDK
+
 sudo mkdir -p /usr/local/lib/java/15
 sudo wget -qO- https://download.java.net/java/GA/jdk15/779bf45e88a44cbd9ea6621d33e33db1/36/GPL/openjdk-15_linux-x64_bin.tar.gz | sudo tar xvz -C /usr/local/lib/java
-#
-#cp ./zshrc ~/.zshrc
+
+cp ./zshrc ~/.zshrc
 
